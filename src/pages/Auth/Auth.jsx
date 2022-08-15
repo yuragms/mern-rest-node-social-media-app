73e1608e24baa372384c7d711103bafdbdebdd0f
yuragms <yura.gms@gmail.com>
2022-08-15 19:24:48 +0300

    end-front

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -12,9 +12,97 @@ const Auth = () => {
           <h6>Explore the ideas throught the world</h6>
         </div>
       </div>
-      <h1>Form</h1>
+
+      <LogIn />
     </div>
   );
 };
+
+function LogIn() {
+  return (
+    <div className="a-right">
+      <form action="" className="infoForm authForm">
+        <h3>Log In</h3>
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            className="infoInput"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            className="infoInput"
+          />
+        </div>
+
+        <div>
+          <span style={{ fontSize: "12px" }}>
+            Don't have an account Sign up!
+          </span>
+          <button className="button infoButton">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+function SignUp() {
+  return (
+    <div className="a-right">
+      <form action="" className="infoForm authForm">
+        <h3>Sign up</h3>
+        <div>
+          <input
+            type="text"
+            placeholder="First name"
+            name="firstname"
+            className="infoInput"
+          />
+          <input
+            type="text"
+            placeholder="Last name"
+            name="lastname"
+            className="infoInput"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Usernames"
+            name="username"
+            className="infoInput"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Password"
+            name="password"
+            className="infoInput"
+          />
+          <input
+            type="text"
+            placeholder="Confirm Password"
+            name="confirmpass"
+            className="infoInput"
+          />
+        </div>
+        <div>
+          <span style={{ fontSize: "12px" }}>
+            Already have an account. Login!
+          </span>
+        </div>
+        <button className="button infoButton" type="submit">
+          Signup
+        </button>
+      </form>
+    </div>
+  );
+}
 
 export default Auth;
